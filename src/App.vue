6d642a4bcc3a0d6f03ba19ec7717ddc71797c2aa
TSSXL <627603421@qq.com>
2019-03-27 17:46:48 +0800

    3-27  font-familt

--- src/App.vue
+++ src/App.vue
@@ -78,7 +78,7 @@ export default {
     border-bottom: 0;
   }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: FZLTHK--GBK1-0;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
